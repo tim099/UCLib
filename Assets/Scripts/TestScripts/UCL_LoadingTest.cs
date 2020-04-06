204a9310c528756cd54c59a8698ebc2fc0249a51
tim1125 <tim11251994@gmail.com>
2020-04-07 00:04:47 +0800

    更新測試場景(新增載入按鈕)

--- Assets/Scripts/TestScripts/UCL_LoadingTest.cs
+++ Assets/Scripts/TestScripts/UCL_LoadingTest.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class UCL_LoadingTest : MonoBehaviour
 {
     [UCL.Core.UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetScenesName")] public string m_LoadScene;
-
+    [UCL.Core.UCL_ButtonProperty] public string Editor_LoadScene = "EditorLoadScene";
+    [UCL.Core.UCL_ButtonProperty(typeof(UCL_LoadingTest))] public int test = 2;
     int timer = 0;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+#if UNITY_EDITOR
+    public void EditorLoadScene() {
+        Debug.LogWarning("EditorLoadScene!!");
+        Debug.LogWarning("EditorLoadScene:" + m_LoadScene);
+        string path = UCL.SceneLib.UCL_Scene.GetScenePath(m_LoadScene);
+        UCL.SceneLib.EditorSceneLoader.LoadScene(path);
+    }
+    public void Test() {
+        Debug.LogWarning("Test()!!");
+    }
+#endif
     // Update is called once per frame
     void Update()
     {
