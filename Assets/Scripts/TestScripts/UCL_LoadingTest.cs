ea57c6ddb1cd63fea312973ee5a638f4bb91e5f3
tim099 <tim11251994@gmail.com>
2020-04-07 17:21:17 +0800

    Update TestScene

--- Assets/Scripts/TestScripts/UCL_LoadingTest.cs
+++ Assets/Scripts/TestScripts/UCL_LoadingTest.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class UCL_LoadingTest : MonoBehaviour
 {
-    [UCL.Core.UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetScenesName")] public string m_LoadScene;
-    [UCL.Core.UCL_ButtonProperty] public string Editor_LoadScene = "EditorLoadScene";
-    [UCL.Core.UCL_ButtonProperty(typeof(UCL_LoadingTest))] public int test = 2;
+    [UCL.Core.PA.UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetScenesName")] public string m_LoadScene;
+    [UCL.Core.PA.UCL_ButtonProperty] public string m_EditorLoadScene;
+    [UCL.Core.PA.UCL_ButtonProperty] public int m_test = 2;
+    [UCL.Core.PA.UCL_ButtonProperty] public UCL_Test m_TestFunc;//(typeof(UCL_Test))
+    [UCL.Core.PA.UCL_ButtonProperty(typeof(UCL_Test), "TestFunc")] public int m_Test = 2;
     int timer = 0;
     // Start is called before the first frame update
     void Start()
@@ -14,14 +16,40 @@ public class UCL_LoadingTest : MonoBehaviour
         
     }
 #if UNITY_EDITOR
+    ///*
     public void EditorLoadScene() {
-        Debug.LogWarning("EditorLoadScene!!");
         Debug.LogWarning("EditorLoadScene:" + m_LoadScene);
         string path = UCL.SceneLib.UCL_Scene.GetScenePath(m_LoadScene);
         UCL.SceneLib.EditorSceneLoader.LoadScene(path);
     }
+    public void EditorLoadScene(int str) {
+        Debug.LogWarning("EditorLoadScene:" + str);
+        Debug.LogWarning("EditorLoadScene:" + m_LoadScene);
+        string path = UCL.SceneLib.UCL_Scene.GetScenePath(m_LoadScene);
+        UCL.SceneLib.EditorSceneLoader.LoadScene(path);
+    }
+    //*/
+    ///*
+    public void EditorLoadScene(string str) {
+        Debug.LogWarning("EditorLoadScene:" + str);
+        Debug.LogWarning("EditorLoadScene:" + m_LoadScene);
+        string path = UCL.SceneLib.UCL_Scene.GetScenePath(m_LoadScene);
+        UCL.SceneLib.EditorSceneLoader.LoadScene(path);
+    }
+    //*/
+    public void EditorLoadScene2() {
+        Debug.LogWarning("EditorLoadScene2:" + m_LoadScene);
+    }
     public void Test() {
         Debug.LogWarning("Test()!!");
+    }
+    public void TestFunc(UCL_Test test) {
+        Debug.LogWarning("Ohh!!");
+        test.TestFunc();
+    }
+    public void test(int vv) {
+        //int vv = 0;
+        Debug.LogWarning("test(ddd)!!:"+vv);
     }
 #endif
     // Update is called once per frame
--- Assets/Scripts/TestScripts/UCL_Test.cs
+++ Assets/Scripts/TestScripts/UCL_Test.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 using UCL.Core;
 public class UCL_Test : MonoBehaviour
 {
-    [UCL_StrListProperty("QAQ", "QWQ")] public string test;
-    [UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetScenesName")] public string test2;
-    [UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetAllScenesName")] public string test3;
+    [UCL.Core.PA.UCL_StrListProperty("QAQ", "QWQ")] public string test;
+    [UCL.Core.PA.UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetScenesName")] public string test2;
+    [UCL.Core.PA.UCL_StrListProperty(typeof(UCL.SceneLib.UCL_Scene), "GetAllScenesName")] public string test3;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public void TestFunc() {
+        Debug.LogWarning("TestFunc:"+name);
+    }
+    static public void TestFunc(int v) {
+        Debug.LogWarning("TestFuncxxx:" + v);
+    }
+    static public void Test(int v) {
+        Debug.LogWarning("Testxxx:" + v);
+    }
     // Update is called once per frame
     void Update()
     {
