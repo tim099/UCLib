45e1a7f1a976069dec7ca5cf7fb9640b8d9f34ab
tim099 <tim11251994@gmail.com>
2020-04-17 17:47:01 +0800

    Add TestScripts/UCL_MicroPhoneTest.cs

--- Assets/Scripts/TestScripts/UCL_StreamingAudioTest.cs
+++ Assets/Scripts/TestScripts/UCL_StreamingAudioTest.cs
@@ -7,23 +7,36 @@ using UCL.AudioLib;
 public class UCL_StreamingAudioTest : MonoBehaviour
 {
     public int m_BufferLen = 4096;
-    public UCL_AudioClipStream m_UCL_AudioClipStream;
+    public UCL_AudioStream m_UCL_AudioStream;
+    //public UCL_MicrophoneStream m_UCL_MicrophoneStream;
     UCL_StreamingAudioSource m_UCL_StreamingAudioSource;
 
     void Start() {
+        m_UCL_AudioStream.Init(m_BufferLen);
+        //m_UCL_MicrophoneStream?.Init(m_BufferLen);
+
         m_UCL_StreamingAudioSource = UCL_StreamingAudioSource.Create(transform);
         
         m_UCL_StreamingAudioSource.GetClip()
-            .SetFrequency(m_UCL_AudioClipStream.m_Clip.frequency)
+            .SetFrequency(m_UCL_AudioStream.m_Frequency)
             .SetLengthSamples(m_BufferLen)
-            .SetChannels(m_UCL_AudioClipStream.m_Clip.channels);
+            .SetChannels(m_UCL_AudioStream.m_Channels);
 
         m_UCL_StreamingAudioSource.Init();
         m_UCL_StreamingAudioSource.Play();
+
+        //System.Buffers.ArrayPool<float>.Shared.Rent(3 * m_BufferLen);
     }
+    public bool m_Pause = false;
     void Update() {
+        if(m_Pause) return;
         if(m_UCL_StreamingAudioSource.GetDataCount() < 2) {
-            m_UCL_StreamingAudioSource.AddData(m_UCL_AudioClipStream.Load(m_BufferLen));
+            //m_UCL_StreamingAudioSource.AddData(m_UCL_AudioClipStream.Load(m_BufferLen));
+            var data = m_UCL_AudioStream.Load();
+            if(data != null) {
+                m_UCL_StreamingAudioSource.AddData(data, m_UCL_AudioStream.Return);
+            }
+            
         }
     }
 }
