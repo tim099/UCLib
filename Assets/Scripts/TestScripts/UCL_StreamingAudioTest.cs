bdeae05731822e07460537924d5f2dc9ccc85c63
tim099 <tim11251994@gmail.com>
2020-04-20 17:22:34 +0800

    Update StreamingAudioDemo.unity

--- Assets/Scripts/TestScripts/UCL_StreamingAudioTest.cs
+++ Assets/Scripts/TestScripts/UCL_StreamingAudioTest.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UCL.AudioLib;
-
-
+using System.Runtime.InteropServices;
+using UCL.Core.MarshalLib;
 public class UCL_StreamingAudioTest : MonoBehaviour
 {
     public int m_BufferLen = 4096;
+    public int m_DataSize;
     public UCL_AudioStream m_UCL_AudioStream;
+    UCL_IntPtr m_Ptr;
     //public UCL_MicrophoneStream m_UCL_MicrophoneStream;
     UCL_StreamingAudioSource m_UCL_StreamingAudioSource;
-
+    UCL_PCMConverter m_UCL_PCMConverter;
+    UCL.Core.Container.UCL_ArrayPoolFixed<float> m_Pool;
+    //float[] src;
+    short[] dst;
+    short[] recieve_dst;
     void Start() {
+        
+
         m_UCL_AudioStream.Init(m_BufferLen);
         //m_UCL_MicrophoneStream?.Init(m_BufferLen);
-
+        m_DataSize = m_UCL_AudioStream.m_Channels * m_BufferLen;
         m_UCL_StreamingAudioSource = UCL_StreamingAudioSource.Create(transform);
         
         m_UCL_StreamingAudioSource.GetClip()
@@ -24,6 +32,29 @@ public class UCL_StreamingAudioTest : MonoBehaviour
 
         m_UCL_StreamingAudioSource.Init();
         m_UCL_StreamingAudioSource.Play();
+        m_UCL_PCMConverter = new UCL_PCMConverter(m_DataSize);
+        m_Ptr = new UCL_IntPtr(sizeof(short) * m_DataSize);//sizeof(short) * 
+        //m_Ptr.Alloc(m_BufferLen * 2);
+        //src = new float[m_BufferLen];
+        dst = new short[m_DataSize];
+        recieve_dst = new short[m_DataSize];
+        m_Pool = new UCL.Core.Container.UCL_ArrayPoolFixed<float>(m_DataSize);
+        //UCL.AudioLib.Lib.PCM_FloatToShort
+        /*
+        unsafe {
+            int[] array = new int[256];
+            int at = 128;
+            int* pArray = (int*)Marshal.UnsafeAddrOfPinnedArrayElement(array, at);
+            int len = array.Length - at;
+            for(int i = 0 ; i < len; i++) {
+                (pArray[i]) = i;
+            }
+            //int[] n_Arr = new int[128](pArray);
+            foreach(var v in array) {
+                Debug.LogWarning(v);
+            }
+        }
+        */
 
         //System.Buffers.ArrayPool<float>.Shared.Rent(3 * m_BufferLen);
     }
@@ -34,7 +65,21 @@ public class UCL_StreamingAudioTest : MonoBehaviour
             //m_UCL_StreamingAudioSource.AddData(m_UCL_AudioClipStream.Load(m_BufferLen));
             var data = m_UCL_AudioStream.Load();
             if(data != null) {
-                m_UCL_StreamingAudioSource.AddData(data, m_UCL_AudioStream.Return);
+                /*
+                UCL.AudioLib.Lib.PCM_FloatToShort(data, dst);
+                m_UCL_AudioStream.Return(data);
+                m_Ptr.ToPtr(dst);
+                m_Ptr.ToArray(recieve_dst);
+                var src = m_Pool.Rent();
+                UCL.AudioLib.Lib.PCM_ShortToFloat(recieve_dst, src);
+                //*/
+                var src = m_Pool.Rent();
+                var ptr = m_UCL_PCMConverter.ToPtr(data);
+                m_UCL_PCMConverter.ToPCM(ptr, src);
+                
+                m_UCL_StreamingAudioSource.AddData(src, m_Pool.Return);
+                
+                //m_UCL_StreamingAudioSource.AddData(data, m_UCL_AudioStream.Return);
             }
             
         }
