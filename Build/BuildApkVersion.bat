62dc7510bcd47e3b6652076cb2f562d5cd12fdfc
tim099 <tim11251994@gmail.com>
2020-04-29 10:15:35 +0800

    Update Build batchs Update UCL

--- Build/BuildApkVersion.bat
+++ Build/BuildApkVersion.bat
@@ -1,28 +1,4 @@
 set /p UnityPath= <UnityInstallPath.txt
 set "BuildSetting=ApkBuildSetting"
-set "TargetPath=%cd%\"
 
-@echo off
-call :GetDirParentN ProjectPath "%TargetPath%" ".."
-echo ProjectPath:            "%ProjectPath%"
-@echo on
-
-"%UnityPath%" -projectPath "%ProjectPath%" -quit -batchmode -executeMethod UCL.BuildLib.UCL_BuildSetting.BuildBySetting -output "%TargetPath%" -logFile build.log -buildsetting "%BuildSetting%"
-
-cmd
-
-
-:GetFileBaseDir
-    :: sets the value to dirFileBase variable
-    set "%~1=%~dp2"
-    exit /b 0
-
-:GetFileBaseDirWithoutEndSlash
-    set "dirWithBackSlash=%~dp2"
-    REM substring from the start to the end minus 1 char from the end
-    set "%~1=%dirWithBackSlash:~0,-1%"
-    exit /b 0
-
-:GetDirParentN
-    for %%I in ("%~2\%~3") do set "%~1=%%~fI"
-    exit /b 0
+call Build.bat "%UnityPath%" "%BuildSetting%"
