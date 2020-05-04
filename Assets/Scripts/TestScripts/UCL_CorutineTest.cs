3a17ed13a13f79fcf3c96da427c7014276070366
tim099 <tim11251994@gmail.com>
2020-05-04 17:36:22 +0800

    Update UCL

--- Assets/Scripts/TestScripts/UCL_CorutineTest.cs
+++ Assets/Scripts/TestScripts/UCL_CorutineTest.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 
 public class UCL_CorutineTest : MonoBehaviour
 {
+    [Serializable]
+    struct test {
+        public int m_T;
+        public string m_S;
+        public float m_F;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        var t = new test();
+        t.m_F = 123.2221f;
+        t.m_S = "Taease";
+        t.m_F = 313232;
+        */
+        LitMiniJson.JsonData t = new LitMiniJson.JsonData();
+        t["m_T"] = 123.2221f;
+        t["m_S"] = "Taease";
+        t["m_F"] = 313232;
+        string str = t.ToJson();//LitMiniJson.JsonMapper.Serialize(t);
+        Debug.LogWarning("Json:" + str);
+        var ss = LitMiniJson.JsonMapper.Deserialize(str);
         StartCoroutine(Test());
         //Application.SetStackTraceLogType
         //Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.ScriptOnly);
