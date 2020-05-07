88713ab204b83ffd05cb6c46d4f0ce0a87771ba1
tim099 <tim11251994@gmail.com>
2020-05-07 17:38:02 +0800

    Update UCL

--- Assets/DemoGames/AOC/AOC_Scripts/AOC_GameManager.cs
+++ Assets/DemoGames/AOC/AOC_Scripts/AOC_GameManager.cs
@@ -8,6 +8,8 @@ namespace AOC {
             if(f_Inited) return;
             base.Init();
             Debug.LogWarning("AOC_GameManager Init!!");
+            Debug.LogWarning("Test4:" + m_GameConfig.GetString("Test4"));
+            m_GameConfig.SetValue("Test4",System.DateTime.Now.ToString("HHmmss"));
         }
     }
 }
