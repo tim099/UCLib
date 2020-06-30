758c0906e99edbf8dedef74e7a3f513d3bdc8fcc
tim099 <tim11251994@gmail.com>
2020-06-30 08:52:26 +0800

    Update 1_ResetToDev.bat

--- 1_ResetToDev.bat
+++ 1_ResetToDev.bat
@@ -1,4 +1,7 @@
 git fetch origin Dev
 git reset --hard origin/Dev
 
+git submodule update --init --remote --recursive
+git submodule foreach -q --recursive "git checkout Dev;git pull origin Dev"
+
 call A_SubmoduleUpdate.bat
