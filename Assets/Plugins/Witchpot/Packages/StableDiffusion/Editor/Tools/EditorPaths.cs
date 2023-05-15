e0b5991022986f4dcf65973bb92ee41ea071cd36
Tim <tim11251994@gmail.com>
2023-05-15 19:00:25 +0800

    Install WitchPot

---
+++ Assets/Plugins/Witchpot/Packages/StableDiffusion/Editor/Tools/EditorPaths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Witchpot.Editor.StableDiffusion
+{
+    public static class EditorPaths
+    {
+        public const string WITCHPOT_ROOT = "Assets/Plugins/Witchpot/";
+        public const string WEBUI_SCRIPT_PATH = "Assets/Plugins/Witchpot/Packages/StableDiffusion/Editor/Tools/run.py";
+
+        public const string WEBUI_PATH = "/witchpot/StableDiffusion.WebUI@1.2.0";
+        //public static string ROOT_PATH => Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+WEBUI_PATH;
+        public static string ROOT_PATH => Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + WEBUI_PATH;
+        public static string WEBUI_SCRIPT_BAT_PATH => ROOT_PATH + "/run.bat";
+        public static string PYTHON_EXE_PATH => ROOT_PATH + "/system/python/python.exe";
+    }
+}
